b9adf578d3def7a201cfc06c05c555881dddeca7
Jugaman <jugamanSWE@proton.me>
2025-04-07 18:12:41 -0300

    Enhancing colors

--- lua/lualine/themes/phantom-gloom.lua
+++ lua/lualine/themes/phantom-gloom.lua
@@ -2,28 +2,28 @@ local colors = require("phantom-gloom.colors").palette
 
 return {
   normal = {
-    a = { fg = colors.night_rider, bg = colors.pastel_green, gui = "bold" },
-    b = { fg = colors.platinum, bg = colors.chinese_black },
-    c = { fg = colors.platinum, bg = colors.vampire_black },
+    a = { fg = colors.night_rider, bg = colors.greenish_turquoise, gui = "bold" },
+    b = { fg = colors.greenish_turqoise, bg = colors.vampire_black },
+    c = { fg = colors.platinum, bg = colors.chinese_black },
   },
   insert = {
-    a = { fg = colors.night_rider, bg = colors.corn, gui = "bold" },
-    b = { fg = colors.platinum, bg = colors.chinese_black },
-    c = { fg = colors.platinum, bg = colors.vampire_black },
+    a = { fg = colors.night_rider, bg = colors.key_lime, gui = "bold" },
+    b = { fg = colors.key_lime, bg = colors.vampire_black },
+    c = { fg = colors.platinum, bg = colors.chinese_black },
   },
   visual = {
-    a = { fg = colors.night_rider, bg = colors.folly, gui = "bold" },
-    b = { fg = colors.platinum, bg = colors.chinese_black },
-    c = { fg = colors.platinum, bg = colors.vampire_black },
+    a = { fg = colors.night_rider, bg = colors.ice_cold, gui = "bold" },
+    b = { fg = colors.ice_cold, bg = colors.vampire_black },
+    c = { fg = colors.platinum, bg = colors.chinese_black },
   },
   replace = {
-    a = { fg = colors.night_rider, bg = colors.ruby_red, gui = "bold" },
-    b = { fg = colors.platinum, bg = colors.chinese_black },
+    a = { fg = colors.night_rider, bg = colors.tulip, gui = "bold" },
+    b = { fg = colors.tulip, bg = colors.chinese_black },
     c = { fg = colors.platinum, bg = colors.vampire_black },
   },
   command = {
-    a = { fg = colors.night_rider, bg = colors.key_lime, gui = "bold" },
-    b = { fg = colors.platinum, bg = colors.chinese_black },
+    a = { fg = colors.night_rider, bg = colors.dark_orchid, gui = "bold" },
+    b = { fg = colors.dark_orchid, bg = colors.chinese_black },
     c = { fg = colors.platinum, bg = colors.vampire_black },
   },
   inactive = {
